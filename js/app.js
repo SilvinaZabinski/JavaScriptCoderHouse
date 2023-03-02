9e3908fbd32f138c3000e9a7154d4d02ce574844
Silvina Zabinski <38991907+SilvinaZabinski@users.noreply.github.com>
2023-03-01 22:33:36 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,26 +1,73 @@
+const productos = [
+	{nombre: "Jeans", precio: 1000 },
+	{nombre: "Remera", precio: 2000 },
+	{nombre: "Shorts", precio: 3000 },
+	{nombre: "Camisa", precio: 4000 },
+	{nombre: "Pollera", precio: 5000 },
+];
 
-	var cantidad;
-	var desc;
-	var compra;
-	var compraTotal; 
-	var pagar;
-while (true){
-	compra = parseFloat(prompt("Ingresar compra"));
-	cantidad = parseFloat(prompt("Ingresar cantidad"));	
-	if (compra =! null)	{
-		alert ("es valido");
-		break;
-	} else { 
-			alert ("no es valido");
-			continue;
-	}
+let carrito = []
+
+let seleccion = prompt ("Ingresar productos a comprar, si o no")
+	
+while(seleccion != "si" && seleccion != "no"){
+	alert ("Por favor ingresar si o no")
+	seleccion = prompt ("Ingresar productos a comprar, si o no")
 }	
-	compraTotal = compra * cantidad;
-	desc = compraTotal*0.15;
-	pagar = compraTotal-desc;
-	
+if (seleccion == "si")
+{
+	alert("Estos son nuestros productos")
+	let listaProductos = productos.map(
+		(productos) => productos.nombre + " " + "$" + productos.precio 
+	);
+	alert(listaProductos.join(" - "))
+} else if (seleccion == "no") 
+{
+	alert ("Vuelva pronto, gracias")
+}
+while (seleccion != "no")
+{
+	let producto = prompt ("Ingresa un producto a tu carrito")
+	let precio = 0
 
-	console.log ("El descuento es: "+desc);
-	console.log ("El total a pagar es: "+pagar);
-	
-	
+	if (producto == "Jeans" || producto == "Remera" || producto == "Shorts" || producto == "Camisa" || producto == "Pollera")
+	{
+		switch(producto)
+		{
+		case "Jeans":
+			precio = 1000;
+			break;
+		case "Remera":
+			precio = 2000;
+			break;
+		case "Shorts":
+			precio = 3000;
+			break;
+		case "Camisa":
+			precio = 4000;
+			break;
+		case "Pollera":
+			precio = 5000;
+			break;
+		default:
+			break;
+
+		}
+	let unidades = parseInt(prompt("Ingrese cantidad de unidades"))	
+	carrito.push({producto, unidades, precio})
+	console.log(carrito)
+	}else{
+		alert ("No disponemos de ese producto")
+	}
+	seleccion = prompt("¿Desea seguir comprando?")
+	while(seleccion === "no")
+	{
+		alert("Gracias por la compra")
+		carrito.forEach((carritoFinal)=>{
+			console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+		})
+		break;
+	}
+}
+const total = carrito.reduce((acc, el)=> acc + el.precio * el.unidades, 0)
+console.log (`el total a pagar por su compra es: ${total}`)
